86f243f6a8552ba080de8cc5e37c28a7a2876888
Bryann <yamildev22@gmail.com>
2026-08-02 22:19:42 -0500

    feat: configuracion inicial del proyecto backend con Spring Boot 4.1, PostgreSQL y docker-compose

---
+++ backend/src/main/java/com/bytr/barberia_api/BarberiaApiApplication.java
@@ -0,0 +1,13 @@
+package com.bytr.barberia_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberiaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberiaApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bytr/barberia_api/BarberiaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytr.barberia_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberiaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
